d6c9aeed3c48238a1ab6bd6cf703124da80c20c4
G <igauravsehrawat@gmail.com>
2019-05-27 20:57:35 +0200

    --add: createServer needs to a function, server.start with cors.

--- src/createServer.js
+++ src/createServer.js
@@ -1,18 +1,19 @@
-const { GraphQLServer } =require('graphql-yoga');
+const { GraphQLServer } = require('graphql-yoga');
 const Mutation = require('./resolvers/Mutation');
 const Query = require('./resolvers/Query');
 const db = require('./db');
 
-const createServer = new GraphQLServer({
-    typeDefs: 'src/schema.graphql',
-    resolvers: {
-        Mutation,
-        Query,
-    },
-    resolverValidatingOptions: {
-        requireResolversForResolveType: false,
-    },
-    context: req => ({ ...req, db }),
-});
-
+function createServer() {
+    return new GraphQLServer({
+        typeDefs: 'src/schema.graphql',
+        resolvers: {
+            Mutation,
+            Query,
+        },
+        resolverValidatingOptions: {
+            requireResolversForResolveType: false,
+        },
+        context: req => ({ ...req, db }),
+    });
+};
 module.exports = createServer;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-// let's go!
+// let's go! yup
+require('dotenv').config({ path: 'variables.env' });
+const createServer = require('./createServer');
+const db = require('./db');
+
+// TODO:Use express middleware for authentication(JWT)
+// TODO: Use express middleware for populating users
+const server = createServer();
+
+server.start({
+    cors: {
+        credentials: true,
+        origin: process.env.FRONTEND_URL,
+    },
+  },
+  deets => {
+    console.log(`Server running now on url
+      localhost:${deets.port}
+    `);
+  },
+);
