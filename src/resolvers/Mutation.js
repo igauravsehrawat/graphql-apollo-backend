c9c0d9fe698d8724b40acacce3c95413fe66a60d
G <igauravsehrawat@gmail.com>
2019-06-01 11:48:47 +0200

    --wip: basics of yoga-graphql

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,3 +1,7 @@
-const Mutations = {};
+const Mutations = {
+  createDog(parent, args, ctx, info) {
+    console.log('The args for the mutation are', args);
+  }
+};
 
 module.exports = Mutations;
--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,3 +1,9 @@
-const Query = {};
+const Query = {
+  // just write the function, type check is done differently
+  // not the typescript way
+  dogs(parent, args, ctx, info) {
+    console.log('The args are', args);
+  }
+}
 
 module.exports = Query;
