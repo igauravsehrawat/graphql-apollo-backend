b55f2d0d2dc747131c937977430e25247f3a102d
G <igauravsehrawat@gmail.com>
2019-06-02 13:19:40 +0200

    --refactor: Easy way.

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,9 +1,7 @@
+const { forwardTo } = require('prisma-binding');
+
 const Query = {
-  async items(parent, args, ctx, info) {
-    // If you see in prisma.graphql there is Query but here we using query
-    const items = await ctx.db.query.items(args, info);
-    return items;
-  }
+  items: forwardTo('db')
 }
 
 module.exports = Query;
