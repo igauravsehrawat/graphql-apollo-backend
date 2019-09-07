7190415e711e7448e4f5d80ec17bf3232f4e4940
Your Name <you@example.com>
2019-09-07 19:02:57 +0200

    --add: attaching userId to request.

--- src/index.js
+++ src/index.js
@@ -3,11 +3,25 @@ require('dotenv').config({ path: 'variables.env' });
 const createServer = require('./createServer');
 const cookieParser = require('cookie-parser');
 const db = require('./db');
+const jwt = require('jsonwebtoken');
 
 // TODO:Use express middleware for authentication(JWT)
 // TODO: Use express middleware for populating users
 const server = createServer();
-server.use(cookieParser());
+server.express.use(cookieParser());
+
+server.express.use((req, res, next) => {
+  console.log(req.cookies);
+  const { token } = req.cookies;
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    if (userId) {
+      // to be used in future for keep track of the user
+      req.userId = userId;
+    }
+  }
+  next();
+});
 
 server.start({
   cors: {
