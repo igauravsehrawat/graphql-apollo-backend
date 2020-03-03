3d68f19dd0fea023f7bc3019f7b6ac37118267ea
G <igauravsehrawat@gmail.com>
2020-03-03 19:52:09 +0100

    --add: order query

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -25,12 +25,33 @@ const Query = {
   },
   users(parent, args, ctx, info) {
     // 1. check if user has permission to query
-    if (!ctx.request.user) {
+    if (!ctx.request.userId) {
       throw Error ('You need to be logged in!!');
     }
     hasPermission(ctx.request.user[0], ['ADMIN', 'PERMISSIONUPDATE']);
     // 2. fetch the cusers
     return ctx.db.query.users({}, info);
+  },
+  async order(parent, args, ctx, info) {
+    // check if user is logged in
+    if (!ctx.request.userId) {
+      throw new Error('You must be logged in!')
+    }
+    // query the order
+    const order = await ctx.db.query.order({
+      where: {
+        id: args.id,
+      }
+    }, info);
+    // info to send the field frontend is asking for
+    // check if they have PERMISSION to see it, hmm
+    const ownPermissions = ctx.request.userId === order.user.id;
+    const hasPermissions = ctx.request.user.permissions.includes('ADMIN');
+    // return the order
+    if (!ownPermissions && !hasPermissions) {
+      throw new Error('You do not have permissions budd');
+    }
+    return order;
   }
 }
 
